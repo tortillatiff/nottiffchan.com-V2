77b0fe370ecd427a8349773c5e0c4d717cef3d4c
tiff <nottiffchan@gmail.com>
2022-02-01 00:44:38 +0800

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,49 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import sadGirl from "../assets/sadgirl.png";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const mainStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  height: "100vh",
+};
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    // <main style={pageStyles}>
+    //   <title>Not found</title>
+    //   <h1 style={headingStyles}>Page not found</h1>
+    //   <p style={paragraphStyles}>
+    //     Sorry{" "}
+    //     <span role="img" aria-label="Pensive emoji">
+    //       😔
+    //     </span>{" "}
+    //     we couldn’t find what you were looking for.
+    //     <br />
+    //     {process.env.NODE_ENV === "development" ? (
+    //       <>
+    //         <br />
+    //         Try creating a page in <code style={codeStyles}>src/pages/</code>.
+    //         <br />
+    //       </>
+    //     ) : null}
+    //     <br />
+    //     <Link to="/">Go home</Link>.
+    //   </p>
+    // </main>
+    <main style={mainStyle}>
+      <img src={sadGirl} style={{ width: "180px" }} />
+      <h1 className="purple-font">Oops...</h1>
+      <p>This page doesn't exist.</p>
+      <AniLink paintDrip hex="#5F3962" to="/">
+        <button className="tiff-btn btn-pri">Go back</button>
+      </AniLink>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
